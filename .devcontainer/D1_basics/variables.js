71e73db0dafd4ebccc0c6d88954e040f3ef9303f
Rajeev Yadav <94947701+rajeevy397@users.noreply.github.com>
2024-04-23 08:39:41 +0000

    variables basics

---
+++ .devcontainer/D1_basics/variables.js
@@ -0,0 +1,14 @@
+const AccountId = 9845861550
+let accountEmail = "rajeev@google.com"
+var accountPassword = "1223"
+accountCity = "Rautahat"
+
+//AccountId = 2
+
+accountEmail = "raj@gmail.com"
+accountCity = "Gaur"
+accountPassword = "444"
+
+console.log(AccountId)
+
+console.table([AccountId,accountEmail,accountPassword,accountCity])
